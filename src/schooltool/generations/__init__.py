2c38454db698669ded11cd37d96a8249a8071a8b
Douglas Cerna <douglascerna@yahoo.com>
2015-07-31 19:18:59 -0600

    Fixed additional reason for leaving

--- src/schooltool/generations/__init__.py
+++ src/schooltool/generations/__init__.py
@@ -22,6 +22,6 @@ Generations for database version upgrades.
 from zope.app.generations.generations import SchemaManager
 
 schemaManager = SchemaManager(
-    minimum_generation=47,
-    generation=47,
+    minimum_generation=48,
+    generation=48,
     package_name='schooltool.generations')
--- src/schooltool/generations/evolve48.py
+++ src/schooltool/generations/evolve48.py
@@ -16,17 +16,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 """
-Upgrade SchoolTool to generation 46.
+Upgrade SchoolTool to generation 48.
 
-Updates leave school demographics fields
+Fixed additional reason for leaving
 """
 
-from zope.app.dependable.interfaces import IDependable
 from zope.app.generations.utility import getRootFolder
 from zope.component.hooks import getSite, setSite
 
-from schooltool.basicperson.demographics import EnumFieldDescription
-from schooltool.basicperson.demographics import TextFieldDescription
 from schooltool.basicperson.interfaces import IDemographicsFields
 from schooltool.common import SchoolToolMessage as _
 
@@ -42,17 +39,5 @@ def evolve(context):
         item = _('Other')
         if item not in dfs[REASON_KEY].items:
             dfs[REASON_KEY].items.append(item)
-            dfs[REASON_KEY]._p_changed = True
-    DESTINATION_KEY = 'leave_destination'
-    if DESTINATION_KEY in dfs:
-        if isinstance(dfs[DESTINATION_KEY], EnumFieldDescription):
-            current_order = list(dfs.keys())
-            title = dfs[DESTINATION_KEY].title
-            limit_keys = dfs[DESTINATION_KEY].limit_keys[:]
-            IDependable(dfs[DESTINATION_KEY]).removePath('')
-            del dfs[DESTINATION_KEY]
-            dfs[DESTINATION_KEY] = TextFieldDescription(
-                DESTINATION_KEY, title, limit_keys=limit_keys)
-            IDependable(dfs[DESTINATION_KEY]).addDependent('')
-            dfs.updateOrder(current_order)
+        dfs[REASON_KEY]._p_changed = True
     setSite(old_site)
